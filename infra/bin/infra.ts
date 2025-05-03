dc97fa7f61ed8c476a520396f7dcbf30b92a58b1
Ehsan Shekari <e.shekari@gmail.com>
2025-05-03 12:25:33 +0300

    chore: ty

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -4,7 +4,8 @@ import * as cdk from "aws-cdk-lib";
 import { InfraStack } from "../lib/infra-stack";
 
 const domainName = process.env.DOMAIN_NAME || "ehsanshekari.com";
-const bucketName = process.env.BUCKET_NAME || "example-website-bucket";
+const bucketName =
+  process.env.BUCKET_NAME || "ehsan-shekari-258-website-bucket";
 
 const app = new cdk.App();
 // new InfraStack(app, 'InfraStack', {
