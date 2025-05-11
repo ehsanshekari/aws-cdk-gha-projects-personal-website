508f168abb9fb45e50ca612b43cb1325f916ddd3
Ehsan Shekari <e.shekari@gmail.com>
2025-05-11 17:26:33 +0300

    chore: test

--- packages/infrastructure/bin/infrastructure.ts
+++ packages/infrastructure/bin/infrastructure.ts
@@ -1,26 +1,26 @@
-// import * as cdk from "aws-cdk-lib";
+import * as cdk from "aws-cdk-lib";
 
-// import { InfrastructureStack } from "../lib/infrastructure-stack";
+import { InfrastructureStack } from "../lib/infrastructure-stack";
 
-// const app = new cdk.App();
+const app = new cdk.App();
 
-// const stageName = "dev";
+const stageName = "dev";
 
-// new InfrastructureStack(app, `oriola-hub-infra-${stageName}`, {
-//   env: {
-//     account: process.env.CDK_DEFAULT_ACCOUNT,
-//     region: process.env.CDK_DEFAULT_REGION,
-//   },
-//   stageName,
-// });
-
-import { App } from "aws-cdk-lib";
-import { PrivateApiStack } from "../lib/private-api-stack";
-
-const app = new App();
-new PrivateApiStack(app, "PrivateApiStack", {
+new InfrastructureStack(app, `oriola-hub-infra-${stageName}`, {
   env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT || "402870043355",
-    region: process.env.CDK_DEFAULT_REGION || "eu-north-1",
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
   },
+  stageName,
 });
+
+// import { App } from "aws-cdk-lib";
+// import { PrivateApiStack } from "../lib/private-api-stack";
+
+// const app = new App();
+// new PrivateApiStack(app, "PrivateApiStack", {
+//   env: {
+//     account: process.env.CDK_DEFAULT_ACCOUNT || "402870043355",
+//     region: process.env.CDK_DEFAULT_REGION || "eu-north-1",
+//   },
+// });
